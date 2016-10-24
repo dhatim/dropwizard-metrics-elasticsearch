bad666fdde4b238d19f0ce0ce23f84b9a38add25
Raphaël Berbain <rberbain@dhatim.com>
2016-10-24 16:22:20 +0200

    travis, maven, sonatype, central, release

---
+++ .travis/script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh -e
+
+if [ ${TRAVIS_PULL_REQUEST} == false -a ${TRAVIS_BRANCH} == master -a ! -z ${TRAVIS_TAG} ]; then
+    # not a pr, branch is master, this is a tag: deploy
+    echo ${GPG_SECRET_KEY} | base64 --decode | gpg --import
+    echo ${GPG_OWNERTRUST} | base64 --decode | gpg --import-ownertrust
+    mvn --settings .travis/settings.xml -P release \
+        -DnewVersion=${TRAVIS_TAG} \
+        org.codehaus.mojo:versions-maven-plugin:2.2:set \
+        deploy
+else
+    # install
+    mvn install
+fi
