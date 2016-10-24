c095c85dcf09d73cfe75973ab1c980031f12586e
Raphaël Berbain <rberbain@dhatim.com>
2016-10-24 18:22:07 +0200

    prepare for live release

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,8 +2,10 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" == false -a -n "${TRAVIS_TAG}" ]; then
     # not a pr & this is a tag? -> deploy
+    set +x
     echo ${GPG_SECRET_KEY} | base64 --decode | gpg --import
     echo ${GPG_OWNERTRUST} | base64 --decode | gpg --import-ownertrust
+    set -x
     mvn versions:set -DnewVersion=${TRAVIS_TAG}
     mvn --settings .travis/settings.xml -P release deploy
 else
