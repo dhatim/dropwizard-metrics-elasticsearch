ea0eea2e3dba4388a568b48d4c28f86250a9a897
Raphaël Berbain <rberbain@dhatim.com>
2016-10-24 17:27:53 +0200

    fix build script

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,10 +4,8 @@ if [ "${TRAVIS_PULL_REQUEST}" == false -a -n "${TRAVIS_TAG}" ]; then
     # not a pr & this is a tag? -> deploy
     echo ${GPG_SECRET_KEY} | base64 --decode | gpg --import
     echo ${GPG_OWNERTRUST} | base64 --decode | gpg --import-ownertrust
-    mvn --settings .travis/settings.xml -P release \
-        -DnewVersion=${TRAVIS_TAG} \
-        org.codehaus.mojo:versions-maven-plugin:2.2:set \
-        deploy
+    mvn versions:set -DnewVersion=${TRAVIS_TAG}
+    mvn --settings .travis/settings.xml -P release deploy
 else
     # install
     mvn install
