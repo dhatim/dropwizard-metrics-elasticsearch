6c6995f08e1437c62c612cc7e6e0a8af17190a09
Raphaël Berbain <rberbain@dhatim.com>
2016-10-25 09:02:25 +0200

    improve travis build

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "${TRAVIS_PULL_REQUEST}" == false -a -n "${TRAVIS_TAG}" ]; then
-    # not a pr & this is a tag? -> deploy
+if [ "${TRAVIS_EVENT_TYPE}" == push ] &&
+       echo "${TRAVIS_TAG}" | egrep '^[0-9]+\.[0-9]+\.[0-9]+$'
+then
+    # the build is triggered by a tag push, and the tag looks like
+    # a version number: proceed with release
     set +x
     echo ${GPG_SECRET_KEY} | base64 --decode | gpg --import
     echo ${GPG_OWNERTRUST} | base64 --decode | gpg --import-ownertrust
     set -x
     mvn versions:set -DnewVersion=${TRAVIS_TAG}
-    mvn --settings .travis/settings.xml -P release deploy
+    mvn -s .travis/settings.xml -P release deploy
 else
-    # install
-    mvn install
+    # this is a regular build
+    mvn -o install
 fi
